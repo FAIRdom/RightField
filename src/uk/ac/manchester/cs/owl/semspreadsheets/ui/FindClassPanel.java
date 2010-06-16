dd8714baf6e5a4a92ab80e640a197e34efd8a526
stuartowen <stuartowen@e2c4a6c7-a36e-4f75-8bb2-dda8f56e1a9f>
2010-06-16 14:36:54 +0000

    added mouse listener to check for double click on a Find result when searching terms
    git-svn-id: https://benchmark.cs.man.ac.uk/svn/DLista/code/owlspreadsheets/trunk@5317 e2c4a6c7-a36e-4f75-8bb2-dda8f56e1a9f

--- src/uk/ac/manchester/cs/owl/semspreadsheets/ui/FindClassPanel.java
+++ src/uk/ac/manchester/cs/owl/semspreadsheets/ui/FindClassPanel.java
@@ -6,6 +6,10 @@ import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseWheelEvent;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -30,132 +34,142 @@ import uk.ac.manchester.cs.owl.semspreadsheets.model.WorkbookManager;
  */
 public class FindClassPanel extends JPanel {
 
-    private WorkbookManager manager;
+	private WorkbookManager manager;
 
-    private JTextField findField = new JTextField();
+	private JTextField findField = new JTextField();
 
-    private JList resultList = new JList();
+	private JList resultList = new JList();
 
-    private JWindow resultWindow;
+	private JWindow resultWindow;
 
-    private WorkbookFrame frame;
+	private WorkbookFrame frame;
 
-    public FindClassPanel(WorkbookFrame frame) {
-        this.frame = frame;
-        this.manager = frame.getWorkbookManager();
-        setLayout(new BorderLayout());
-        findField.putClientProperty("JTextField.variant", "search");
-        add(findField, BorderLayout.NORTH);
-        findField.getDocument().addDocumentListener(new DocumentListener() {
-            public void insertUpdate(DocumentEvent e) {
-                doFind();
-            }
+	public FindClassPanel(WorkbookFrame frame) {
+		this.frame = frame;
+		this.manager = frame.getWorkbookManager();
+		setLayout(new BorderLayout());
+		findField.putClientProperty("JTextField.variant", "search");
+		add(findField, BorderLayout.NORTH);
+		findField.getDocument().addDocumentListener(new DocumentListener() {
+			public void insertUpdate(DocumentEvent e) {
+				doFind();
+			}
 
-            public void removeUpdate(DocumentEvent e) {
-                doFind();
-            }
+			public void removeUpdate(DocumentEvent e) {
+				doFind();
+			}
 
-            public void changedUpdate(DocumentEvent e) {
-            }
-        });
-        resultWindow = new JWindow(frame);
-        resultWindow.getContentPane().setLayout(new BorderLayout());
-        resultWindow.getContentPane().add(new JScrollPane(resultList));
-        resultList.setCellRenderer(new WorkbookManagerCellRenderer(manager));
-        resultList.setRequestFocusEnabled(false);
-        resultWindow.setFocusable(false);
-        resultWindow.setFocusableWindowState(false);
-        findField.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-                    dismiss();
-                }
-                else if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    navigate();
-                }
-            }
-        });
-        findField.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode() == KeyEvent.VK_UP) {
-                    moveUpList();
-                }
-                else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-                    moveDownList();
-                }
-            }
-        });
-        findField.addFocusListener(new FocusAdapter() {
-            public void focusLost(FocusEvent e) {
-                dismiss();
-            }
-        });
-    }
+			public void changedUpdate(DocumentEvent e) {
+			}
+		});
+		resultWindow = new JWindow(frame);
+		resultWindow.getContentPane().setLayout(new BorderLayout());
+		resultWindow.getContentPane().add(new JScrollPane(resultList));
+		resultList.setCellRenderer(new WorkbookManagerCellRenderer(manager));
+		resultList.setRequestFocusEnabled(false);
+		resultWindow.setFocusable(false);
+		resultWindow.setFocusableWindowState(false);
+		findField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+					dismiss();
+				} else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					navigate();
+				}
+			}
+		});
+		findField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_UP) {
+					moveUpList();
+				} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+					moveDownList();
+				}
+			}
+		});
+		findField.addFocusListener(new FocusAdapter() {
+			public void focusLost(FocusEvent e) {
+				dismiss();
+			}
+		});
+		resultList.addMouseListener(new MouseAdapter() {			
 
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount()>1) { //requires double click
+					navigate();
+				}
+			}			
+		});
+	}
 
-    private void moveUpList() {
-        int selIndex = resultList.getSelectedIndex();
-        selIndex--;
-        if(selIndex < 0) {
-            selIndex = resultList.getModel().getSize() - 1;
-        }
-        if(selIndex >= 0) {
-            resultList.setSelectedIndex(selIndex);
-            resultList.scrollRectToVisible(resultList.getCellBounds(selIndex, selIndex));
-        }
-    }
+	private void moveUpList() {
+		int selIndex = resultList.getSelectedIndex();
+		selIndex--;
+		if (selIndex < 0) {
+			selIndex = resultList.getModel().getSize() - 1;
+		}
+		if (selIndex >= 0) {
+			resultList.setSelectedIndex(selIndex);
+			resultList.scrollRectToVisible(resultList.getCellBounds(selIndex,
+					selIndex));
+		}
+	}
 
-    private void moveDownList() {
-        if (resultList.getModel().getSize() > 0) {
-            int selIndex = resultList.getSelectedIndex();
-            selIndex++;
-            if(selIndex > resultList.getModel().getSize() - 1) {
-                selIndex = 0;
-            }
-            resultList.setSelectedIndex(selIndex);
-            resultList.scrollRectToVisible(resultList.getCellBounds(selIndex, selIndex));
-        }
-    }
+	private void moveDownList() {
+		if (resultList.getModel().getSize() > 0) {
+			int selIndex = resultList.getSelectedIndex();
+			selIndex++;
+			if (selIndex > resultList.getModel().getSize() - 1) {
+				selIndex = 0;
+			}
+			resultList.setSelectedIndex(selIndex);
+			resultList.scrollRectToVisible(resultList.getCellBounds(selIndex,
+					selIndex));
+		}
+	}
 
-    private void dismiss() {
-        resultWindow.setVisible(false);
-    }
+	private void dismiss() {
+		resultWindow.setVisible(false);
+	}
 
-    private void navigate() {
-        Object o = resultList.getSelectedValue();
-        if(o != null) {
-            manager.getEntitySelectionModel().setSelection((OWLEntity) o);
-        }
-        resultWindow.setVisible(false);
-    }
+	private void navigate() {
+		Object o = resultList.getSelectedValue();
+		if (o != null) {
+			manager.getEntitySelectionModel().setSelection((OWLEntity) o);
+		}
+		resultWindow.setVisible(false);
+	}
 
-    private void doFind() {
-        Collection<OWLEntity> entities = manager.getEntitiesForShortForm(findField.getText().trim());
-        ArrayList<OWLEntity> sortedEntities = new ArrayList<OWLEntity>();
-        for(OWLEntity ent : entities) {
-            if(ent.isOWLClass()) {
-                sortedEntities.add(ent);
-            }
-        }
-        resultList.setListData(sortedEntities.toArray());
-        showResults();
-    }
+	private void doFind() {
+		Collection<OWLEntity> entities = manager
+				.getEntitiesForShortForm(findField.getText().trim());
+		ArrayList<OWLEntity> sortedEntities = new ArrayList<OWLEntity>();
+		for (OWLEntity ent : entities) {
+			if (ent.isOWLClass()) {
+				sortedEntities.add(ent);
+			}
+		}
+		resultList.setListData(sortedEntities.toArray());
+		showResults();
+	}
 
-    private void showResults() {
-        if (resultList.getModel().getSize() > 0) {
-            resultWindow.setSize(findField.getWidth(), 150);
-            Point point = new Point(0, 0);
-            SwingUtilities.convertPointToScreen(point, findField);
-//            SwingUtilities.convertPointFromScreen(point, frame.getContentPane());
-            resultWindow.setLocation(point.x, point.y + findField.getHeight() + 3);
-            resultWindow.setVisible(true);
-            resultList.setSelectedIndex(0);
-        }
-        else {
-            resultWindow.setVisible(false);
-        }
-    }
+	private void showResults() {
+		if (resultList.getModel().getSize() > 0) {
+			resultWindow.setSize(findField.getWidth(), 150);
+			Point point = new Point(0, 0);
+			SwingUtilities.convertPointToScreen(point, findField);
+			// SwingUtilities.convertPointFromScreen(point,
+			// frame.getContentPane());
+			resultWindow.setLocation(point.x, point.y + findField.getHeight()
+					+ 3);
+			resultWindow.setVisible(true);
+			resultList.setSelectedIndex(0);
+		} else {
+			resultWindow.setVisible(false);
+		}
+	}
 
 }
