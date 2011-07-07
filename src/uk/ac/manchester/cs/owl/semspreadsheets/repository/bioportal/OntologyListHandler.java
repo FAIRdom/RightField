3aa1ddb14cc7aa41af51116b1922b5fa1d29d188
Stuart Owen <sowen@cs.man.ac.uk>
2011-07-07 11:35:45 +0100

    filtering out BioPortal ontologies based upon a white list of supported formats

--- src/uk/ac/manchester/cs/owl/semspreadsheets/repository/bioportal/OntologyListHandler.java
+++ src/uk/ac/manchester/cs/owl/semspreadsheets/repository/bioportal/OntologyListHandler.java
@@ -1,14 +1,22 @@
 package uk.ac.manchester.cs.owl.semspreadsheets.repository.bioportal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
- * Author: Matthew Horridge<br>
+ * Author: Matthew Horridge, Stuart Owen<br>
  * The University of Manchester<br>
  * Information Management Group<br>
  * Date: 11-Nov-2009
+ * 
+ * @author Stuart Owen
+ * @author Matthew Horridge
  */
 public class OntologyListHandler extends DefaultHandler {
 
@@ -17,6 +25,10 @@ public class OntologyListHandler extends DefaultHandler {
     public static final String DISPLAY_LABEL_ELEMENT_NAME = "displayLabel";
     
     public static final String FORMAT_ELEMENT_NAME = "format";
+    
+    public static final String BEAN_ELEMENT_NAME = "ontologyBean";
+    
+    private static final List<String> FORMAT_WHITE_LIST = Arrays.asList(new String []{"OWL","OBO","OWL-FULL","OWL-DL"});
 
     private NullElementState nullElementState = new NullElementState();
 
@@ -55,9 +67,28 @@ public class OntologyListHandler extends DefaultHandler {
             elementState = nullElementState;
         }
     }
+    
+    
 
 
     @Override
+	public void endElement(String uri, String localName, String qName)
+			throws SAXException {
+		if (qName.equals(BEAN_ELEMENT_NAME)) {
+			if (acceptableFormat(lastFormat)) {
+				itemHandler.handleItem(new BioPortalRepositoryItem(lastOntologyID, lastDisplayLabel,lastFormat));
+			}			
+			lastOntologyID=0;
+			lastDisplayLabel=null;
+			lastFormat=null;
+		}
+	}
+
+	private boolean acceptableFormat(String format) {
+		return FORMAT_WHITE_LIST.contains(format.toUpperCase());
+	}
+
+	@Override
     public void characters(char[] ch, int start, int length) throws SAXException {
         elementState.characters(ch, start, length);
     }
@@ -91,8 +122,7 @@ public class OntologyListHandler extends DefaultHandler {
     private class DisplayLabelState implements ElementState {
 
         public void characters(char[] ch, int start, int length) throws SAXException {
-            lastDisplayLabel = new String(ch, start, length);
-            itemHandler.handleItem(new BioPortalRepositoryItem(lastOntologyID, lastDisplayLabel,lastFormat));
+            lastDisplayLabel = new String(ch, start, length);            
         }
     }
 }
