c6d3baef816d911ec1ccb3b797c3709ec5072b37
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-01-09 16:25:15 +0000

    added a timeout of 30 seconds when waiting for BioPortal

--- src/main/java/uk/ac/manchester/cs/owl/semspreadsheets/repository/bioportal/OntologyListAccessor.java
+++ src/main/java/uk/ac/manchester/cs/owl/semspreadsheets/repository/bioportal/OntologyListAccessor.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -28,6 +29,8 @@ import uk.ac.manchester.cs.owl.semspreadsheets.ui.ErrorHandler;
  */
 public class OntologyListAccessor {
 	
+	private final int CONNECT_TIMEOUT = 30000;
+	
 	private static Logger logger = Logger.getLogger(OntologyListAccessor.class);
 
     public Collection<BioPortalRepositoryItem> getOntologies() {
@@ -47,9 +50,11 @@ public class OntologyListAccessor {
                 }
             });            
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setConnectTimeout(CONNECT_TIMEOUT);
+            connection.setReadTimeout(CONNECT_TIMEOUT);
             if (connection.getResponseCode()==403) {
             	throw new BioPortalAccessDeniedException();
-            }
+            }            
             BufferedInputStream bufferedInputStream = new BufferedInputStream(connection.getInputStream());
             saxParser.parse(bufferedInputStream, handler);
             bufferedInputStream.close();
@@ -66,12 +71,16 @@ public class OntologyListAccessor {
         catch (MalformedURLException e) {
             logger.error("Error with URL for BioPortal rest API",e);
         }
+        catch (SocketTimeoutException e) {
+        	logger.error("Timeout connecting to BioPortal",e);
+        	ErrorHandler.getErrorHandler().handleError(e);
+        }
         catch (IOException e) {        	
             logger.error("Error communiciating with BioPortal rest API",e);                    	
         }
         catch (BioPortalAccessDeniedException e) {
         	ErrorHandler.getErrorHandler().handleError(e);
-        }
+        }        
         return items;
     }
 }
