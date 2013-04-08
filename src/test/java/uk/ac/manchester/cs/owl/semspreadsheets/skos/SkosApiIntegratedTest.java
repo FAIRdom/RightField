e3c26c793c794d62e7a7417b3ec8b845d5e88235
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-04-08 13:40:15 +0100

    experimenting with accessign the SKOSManager and concepts by using the OWLOntology manager, which will make integration easier

--- src/test/java/uk/ac/manchester/cs/owl/semspreadsheets/skos/SkosApiIntegratedTest.java
+++ src/test/java/uk/ac/manchester/cs/owl/semspreadsheets/skos/SkosApiIntegratedTest.java
@@ -7,12 +7,18 @@ import java.net.URI;
 import java.util.Set;
 
 import org.junit.Test;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.skos.SKOSAnnotation;
 import org.semanticweb.skos.SKOSConcept;
 import org.semanticweb.skos.SKOSDataset;
+import org.semanticweb.skos.properties.SKOSTopConceptOfProperty;
 import org.semanticweb.skosapibinding.SKOSManager;
+import org.semanticweb.skosapibinding.SKOStoOWLConverter;
 
 import uk.ac.manchester.cs.owl.semspreadsheets.DocumentsCatalogue;
+import uk.ac.manchester.cs.owl.semspreadsheets.model.OntologyManager;
+import uk.ac.manchester.cs.owl.semspreadsheets.model.WorkbookManager;
 
 public class SkosApiIntegratedTest {
 
@@ -46,5 +52,19 @@ public class SkosApiIntegratedTest {
 		SKOSConcept concept = manager.getSKOSDataFactory().getSKOSConcept(URI.create("http://onto.nerc.ac.uk/CAST/11"));
 		assertNotNull(concept);
 		assertEquals(12,concept.getSKOSAnnotations(dataset).size());
+	}
+	
+	@Test
+	public void testLoadingUsignOWLOntologyManager() throws Exception {
+		WorkbookManager workbookManager = new WorkbookManager();
+		OntologyManager ontologyManager = workbookManager.getOntologyManager();	
+		OWLOntologyManager owlOntologyManager = ontologyManager.getOWLOntologyManager();
+		ontologyManager.loadOntology(DocumentsCatalogue.uriForResourceName("skos/skos-example.rdf"));
+		ontologyManager.loadOntology(DocumentsCatalogue.jermOntologyURI());
+		SKOSManager manager = new SKOSManager(owlOntologyManager);
+		SKOSConcept concept = manager.getSKOSDataFactory().getSKOSConcept(URI.create("http://www.fluffyboards.com/vocabulary#snowboard"));
+		assertNotNull(concept);				
+		
+		
 	}
 }
