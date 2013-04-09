bfc3a637b3c0af367c9b936e6ee7b5af7d6e896f
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-04-09 11:23:42 +0100

    Experimenting with using Pellet as the OWL reasoner for use with the SKOSReasoner, still not working
    (committed for future reference)

--- src/test/java/uk/ac/manchester/cs/owl/semspreadsheets/skos/SkosApiIntegratedTest.java
+++ src/test/java/uk/ac/manchester/cs/owl/semspreadsheets/skos/SkosApiIntegratedTest.java
@@ -7,7 +7,9 @@ import java.net.URI;
 import java.util.Set;
 
 import org.junit.Test;
-import org.semanticweb.HermiT.Reasoner;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLImportsDeclaration;
+import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.skos.SKOSAnnotation;
@@ -16,6 +18,9 @@ import org.semanticweb.skos.SKOSDataset;
 import org.semanticweb.skosapibinding.SKOSManager;
 import org.semanticweb.skosapibinding.SKOSReasoner;
 import org.semanticweb.skosapibinding.SKOStoOWLConverter;
+
+import com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory;
+import com.clarkparsia.pellet.owlapiv3.Reasoner;
 
 import uk.ac.manchester.cs.owl.semspreadsheets.DocumentsCatalogue;
 import uk.ac.manchester.cs.owl.semspreadsheets.model.OntologyManager;
@@ -74,26 +79,93 @@ public class SkosApiIntegratedTest {
 	@Test
 	public void testReasoningToGetHierarchy() throws Exception {
 		SKOSManager manager = new SKOSManager();
+		//manager.loadDatasetFromPhysicalURI(DocumentsCatalogue.uriForResourceName("skos/skos-owl1-dl.rdf"));
+		
 		URI uri = DocumentsCatalogue.uriForResourceName("skos/skos-example.rdf");
+		
 		SKOSDataset dataset = manager.loadDataset(uri);
-		//manager.loadDataset(DocumentsCatalogue.uriForResourceName("skos/skos-owl1-dl.rdf"));
-		
-		SKOSConcept concept = manager.getSKOSDataFactory().getSKOSConcept(URI.create("http://www.fluffyboards.com/vocabulary#snowboard"));
-		Set<SKOSAnnotation> skosAnnotations = concept.getSKOSAnnotationsByURI(dataset, URI.create("http://www.w3.org/2004/02/skos/core#broader"));
-		assertEquals(1,skosAnnotations.size());
-		assertEquals("http://www.fluffyboards.com/vocabulary#product",skosAnnotations.iterator().next().getAnnotationValue().getURI().toString());
 				
-		SKOStoOWLConverter converter = new SKOStoOWLConverter(); 
-		OWLReasoner r = new Reasoner.ReasonerFactory().createReasoner(converter.getAsOWLOntology(dataset));
-		SKOSReasoner reasoner = new SKOSReasoner(manager, r);
-		for (SKOSConcept concept2 : reasoner.getSKOSConcepts()) {
-			System.out.println(concept2.getURI());
-		}
-		skosAnnotations = reasoner.getSKOSAnnotation(concept, URI.create("http://www.w3.org/2004/02/skos/core#broader"));
-		assertEquals(1,skosAnnotations.size());
-		assertEquals("http://www.fluffyboards.com/vocabulary#product",skosAnnotations.iterator().next().getAnnotationValue().getURI().toString());
+				
+		SKOStoOWLConverter converter = new SKOStoOWLConverter();
 		
-		Set<SKOSConcept> skosBroaderConcepts = reasoner.getSKOSBroaderConcepts(concept);
-		assertEquals(1,skosBroaderConcepts.size());
+		OWLImportsDeclaration importsDec = 
+				manager.getOWLManger().getOWLDataFactory().getOWLImportsDeclaration(IRI.create 
+				("http://www.w3.org/2004/02/skos/core"));
+		
+		OWLReasoner r = PelletReasonerFactory.getInstance().createReasoner(converter.getAsOWLOntology(dataset));
+		//OWLReasoner r = new Reasoner.ReasonerFactory().createReasoner(converter.getAsOWLOntology(dataset));
+		SKOSReasoner reasoner = new SKOSReasoner(manager, r);
+		for (SKOSConcept concept : reasoner.getSKOSConcepts()) {
+			System.out.println(concept.getURI());
+			 for (SKOSConcept broaderCon : 
+				 reasoner.getSKOSBroaderTransitiveConcepts(concept)) { 
+				                     for (SKOSAnnotation literal : 
+				 broaderCon.getSKOSAnnotationsByURI(dataset, 
+				 manager.getSKOSDataFactory().getSKOSPrefLabelProperty().getURI())) { 
+				                         System.out.println("Narrower concepts: " + 
+				 literal.getAnnotationValueAsConstant().getLiteral()); 
+				                     }
+			 }
+//			if (concept.getURI().toString()=="http://www.fluffyboards.com/vocabulary#snowboard") {
+//				System.out.println(concept.);
+//			}
+		}
+//		skosAnnotations = reasoner.getSKOSAnnotation(concept, URI.create("http://www.w3.org/2004/02/skos/core#broader"));
+//		assertEquals(1,skosAnnotations.size());
+//		assertEquals("http://www.fluffyboards.com/vocabulary#product",skosAnnotations.iterator().next().getAnnotationValue().getURI().toString());
+//		
+//		Set<SKOSConcept> skosBroaderConcepts = reasoner.getSKOSBroaderConcepts(concept);
+//		assertEquals(1,skosBroaderConcepts.size());
+	}
+	
+	@Test
+	public void testReasoningWithPellet() throws Exception {
+		SKOSManager manager = new SKOSManager();
+		SKOSDataset skosCoreOntology = manager.loadDataset(DocumentsCatalogue
+				.uriForResourceName("skos/skos-owl1-dl.rdf"));
+		SKOSDataset dataSet = manager.loadDataset(DocumentsCatalogue
+				.uriForResourceName("skos/skos-example.rdf"));
+		SKOStoOWLConverter converter = new SKOStoOWLConverter();
+		OWLOntology mySkosAsOWLOntology = converter.getAsOWLOntology(dataSet);
+		// your skos dataset needs to import the skos core ontology, we do this
+		// using the OWL API
+		OWLImportsDeclaration importsDec = manager
+				.getOWLManger()
+				.getOWLDataFactory()
+				.getOWLImportsDeclaration(
+						IRI.create("http://www.w3.org/2004/02/skos/core"));
+		// create the pellet reasoner
+		OWLReasoner reasoner = new com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory()
+				.createReasoner(converter.getAsOWLOntology(dataSet));
+		// pass the pellet reasoner and the SKOS ontology file to the
+		// SKOSReasoner
+		SKOSReasoner skosreasoner = new SKOSReasoner(manager, reasoner,
+				converter.getAsOWLOntology(skosCoreOntology));
+		// example of getting inferred skos:narrowerTransitive concepts
+		for (SKOSConcept con : skosreasoner.getSKOSConcepts()) {
+			System.out.println("Concept:" + con.getURI());
+			for (SKOSConcept broaderCon : skosreasoner
+					.getSKOSNarrowerConcepts(con)) {
+				for (SKOSAnnotation literal : broaderCon
+						.getSKOSAnnotationsByURI(dataSet, manager
+								.getSKOSDataFactory()
+								.getSKOSPrefLabelProperty().getURI())) {
+					System.out.println("Narrower concepts: "
+							+ literal.getAnnotationValueAsConstant()
+									.getLiteral());
+				}
+			}
+			for (SKOSConcept broaderCon : skosreasoner
+					.getSKOSBroaderTransitiveConcepts(con)) {
+				for (SKOSAnnotation literal : broaderCon
+						.getSKOSAnnotationsByURI(dataSet, manager
+								.getSKOSDataFactory()
+								.getSKOSPrefLabelProperty().getURI())) {
+					System.out.println("Broader concepts: "
+							+ literal.getAnnotationValueAsConstant()
+									.getLiteral());
+				}
+			}
+		}
 	}
 }
