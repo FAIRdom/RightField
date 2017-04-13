a9154f5cec24e569d7601cc058d7f29aae28a107
alaninmcr <alan.r.williams@manchester.ac.uk>
2017-04-13 16:16:49 +0100

    Update opencsv dependency

--- src/main/java/uk/ac/manchester/cs/owl/semspreadsheets/repository/bioportal/LegacyBioportalSourceResolver.java
+++ src/main/java/uk/ac/manchester/cs/owl/semspreadsheets/repository/bioportal/LegacyBioportalSourceResolver.java
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.semanticweb.owlapi.model.IRI;
 
-import au.com.bytecode.opencsv.CSVReader;
+import com.opencsv.CSVReader;
 
 
 /** 
